62b9dc3d7d3740ab6c66de47bfcac6c6bce28a63
xiaojun.zhang <xiaojun.zhang@bestwehotel.com>
2018-12-19 10:47:41 +0800

    #N/A[xiaojz]

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,8 +1,9 @@
+import '@babel/polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader' // eslint-disable-line
 import { ReduxAsyncConnect } from 'redux-connect'
 import { loadableReady } from '@loadable/component'
 import createStore from './store'
@@ -13,11 +14,10 @@ import './assets/styles/index.styl'
 const initialState = {} // eslint-disable-line
 const helpers = { api }
 const store = createStore(initialState)
-
 store.dispatch.api.initApi(api)
 
 const renderApp = (routes) => {
-    ReactDOM.hydrate(
+    ReactDOM.render(
         <AppContainer>
             <Provider store={store}>
                 <Router>
@@ -38,10 +38,5 @@ if (module.hot) {
     module.hot.accept('./routes', () => {
         const routerConfig = require('./routes').default // eslint-disable-line
         renderApp(routesConfig)
-    })
-    module.hot.accept(err => {
-        if (err) {
-            console.error('Cannot apply HMR updata', err)
-        }
     })
 }
